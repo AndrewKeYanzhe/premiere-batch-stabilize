0fd69ffc53f8270b61b16b858dc7bdd07dcecba6
AndrewKeYanzhe <keyanzhe@gmail.com>
2023-08-31 03:51:44 +0800

    open and close project

--- stabilize.py
+++ stabilize.py
@@ -9,13 +9,36 @@ new_clip = "/Users/andrewke/Desktop/100D Test/M26-1041_2.8K_waist quite a bit of
 #to run environment
 # source /Users/andrewke/Documents/Pymiere/pymiere/bin/activate
 
-to_stabilize = False
-to_render = False
-to_import = False
+# to_stabilize = True
+to_render = True
+to_import = True
 
 import time
 import pymiere
 from pymiere import wrappers
+
+
+import pymiere  
+project_path = "/Users/andrewke/Desktop/100D Test/Untitled 4.prproj"
+# create new empty project  
+# pymiere.objects.app.newProject(project_path)  # from Premiere 2020  
+# pymiere.objects.qe.newProject(project_path)  # before Premiere 2020  
+# open existing project  
+pymiere.objects.app.openDocument(project_path)  
+# save project  
+# pymiere.objects.app.project.save()  
+# pymiere.objects.app.project.saveAs(project_path)  
+# close project  
+
+
+
+project = pymiere.objects.app.project
+
+
+# sequence = project.createNewSequence("Sequence 01","0")
+
+
+
 
 # Check for an open project
 project_opened, sequence_active = wrappers.check_active_sequence(crash=False)
@@ -32,7 +55,6 @@ if not sequence_active:
     project.activeSequence = sequences[0]
 
 
-project = pymiere.objects.app.project
 
 sequence = project.activeSequence
 
@@ -112,22 +134,6 @@ print("Sequence as a framerate of {} fps".format(fps))
 # Select the first video clip in the Timeline
 clips[0].setSelected(True, True)
 
-# The following code will not work in Premiere Pro 2017 (clips were not editable at the time)
-# Periodically advance the first video clip through the Timeline
-start_frame = 0
-end_frame = 100
-# for i in range(30):
-#     increment = i * 5
-#     wrappers.edit_clip(clips[0], start_frame + increment, end_frame + increment, start_frame, end_frame, fps=fps)
-#     time.sleep(0.1)
-
-# Set sequence resolution
-
-# sequence.setSettings("videoFrameHeight"=4800)
-
-
-
-
 
 
 
@@ -145,8 +151,10 @@ def getComponentByDisplayName(components, name):
 
 def applyEffectProperties(component):
     if not component:
+        print("not component")
         return
     
+    print("adding warp stabilizer effect")
     isDetailedAnalysis=False
     
     for property in component.properties:
@@ -164,11 +172,29 @@ def applyEffectProperties(component):
 
     return
 
+
+
+if not sequence_active:
+    sequences = wrappers.list_sequences()
+    for seq in sequences:
+        project.openSequence(sequenceID=seq.sequenceID)
+    # Set the first Sequence in the list as the active Sequence
+    project.activeSequence = sequences[0]
+
+
 effectName = "Warp Stabilizer"
 qsequence = pymiere.objects.qe.project.getSequenceAt(0)
 # qsequence = sequence
 qclip = qsequence.getVideoTrackAt(0).getItemAt(0)
-if to_stabilize: qclip.addVideoEffect(pymiere.objects.qe.project.getVideoEffectByName("Warp Stabilizer"))
+
+
+
+print("qclip",qclip)
+
+
+
+
+qclip.addVideoEffect(pymiere.objects.qe.project.getVideoEffectByName("Warp Stabilizer"))
 smoothness = 2
 applyEffectProperties(getComponentByDisplayName(clip.components, effectName))
 
@@ -177,6 +203,9 @@ applyEffectProperties(getComponentByDisplayName(clip.components, effectName))
 while not sequence.isDoneAnalyzingForVideoEffects():
     time.sleep(1)  # Sleep for 1 second
 
+
+
+# time.sleep(99999)
 outputPath = "/Users/andrewke/Downloads/out.mp4"
 
 
@@ -186,3 +215,7 @@ if to_render:
         "/Users/andrewke/Documents/Adobe/Adobe Media Encoder/22.0/Presets/hevc10.epr",  # path of the export preset file
         pymiere.objects.app.encoder.ENCODE_ENTIRE  # what part of the sequence to export. Others are: ENCODE_IN_TO_OUT or ENCODE_WORKAREA
     )
+
+pymiere.objects.app.project.saveAs("/Users/andrewke/Desktop/100D Test/Untitled 5.prproj")  
+
+pymiere.objects.app.project.closeDocument()
